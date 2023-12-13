4deb6271ec91a5a3c707352562895061b60672f7
Aimeexu <1062303846@qq.com>
2023-12-13 11:39:05 +0800

    init

---
+++ Example/LKJsApiExternalIMP/TFViewController.h
@@ -0,0 +1,13 @@
+//
+//  TFViewController.h
+//  LKJsApiExternalIMP
+//
+//  Created by Aimeexu on 12/13/2023.
+//  Copyright (c) 2023 Aimeexu. All rights reserved.
+//
+
+@import UIKit;
+
+@interface TFViewController : UIViewController
+
+@end
---
+++ Example/Pods/Target Support Files/Pods-LKJsApiExternalIMP_Tests/Pods-LKJsApiExternalIMP_Tests-umbrella.h
@@ -0,0 +1,16 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+
+FOUNDATION_EXPORT double Pods_LKJsApiExternalIMP_TestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_LKJsApiExternalIMP_TestsVersionString[];
+
